e2b30d4cab13b33d27579d869ba69c124ecce393
Skirmantas Darbas <skirmantas596@gmail.com>
2025-04-10 15:53:52 +0300

    Completed architecture

--- src/gateways/ToDoGateway.tsx
+++ src/gateways/ToDoGateway.tsx
@@ -3,12 +3,13 @@ import { Todo } from "../types/ToDoTypes";
 const STORAGE_KEY = "todos";
 
 export const todoGateway = {
-  getTodos(): Todo[] {
-    const data = localStorage.getItem(STORAGE_KEY);
-    return data ? JSON.parse(data) : [];
-  },
+	getTodos(): Todo[] {
+		const data = localStorage.getItem(STORAGE_KEY);
+		return data ? JSON.parse(data) : [];
+	},
 
-  saveTodos(todos: Todo[]) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-  }
+	saveTodos(todos: Todo[]) {
+			
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+	}
 };
